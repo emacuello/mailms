a7ef71df042e250a2a1264d9847ed73691efa09f
Victor Emanuel Cuello <84103575+emacuello@users.noreply.github.com>
2024-07-03 15:13:42 -0300

    limpiando para el deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,11 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MailModule } from './mail/mail.module';
-import { RejectHttpMiddleware } from './guard/forbidden.guard';
 
 @Module({
   imports: [MailModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(RejectHttpMiddleware).forRoutes('*');
-  }
-}
+export class AppModule {}
